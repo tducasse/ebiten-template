5b684c0a8b40de40a1a3aed07bc4bad6353e266d
Thibaud Ducasse <thibaud.ducasse@gmail.com>
2021-09-01 22:37:53 +1000

    Add input

---
+++ input/input.go
@@ -0,0 +1,31 @@
+package input
+
+import (
+	"log"
+
+	"github.com/hajimehoshi/ebiten/v2"
+)
+
+var Input map[string][]ebiten.Key
+
+func IsPressed(action string) bool {
+	keys, ok := Input[action]
+	if !ok {
+		log.Println("Action " + action + " is not mapped")
+		return false
+	}
+	for _, k := range keys {
+		if p := ebiten.IsKeyPressed(k); p {
+			return true
+		}
+	}
+	return false
+}
+
+func Init(keys map[string][]ebiten.Key) {
+	Input = keys
+}
+
+func AddAction(action string, keys []ebiten.Key) {
+	Input[action] = keys
+}
